62ff018ca350a56095d6457f9990cdd18dd9a588
umgbhalla <umg.bhalla88@gmail.com>
2022-01-30 03:06:56 +0530

    feat(base): added local deploy script

---
+++ dev-deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/env bash
+docker-compose up -d --build 
+http POST :3000/patients first_name="Richard" last_name="Smith" email="richarsd@email.com" phone=123456789 address="123 Main St" diagnosis="Light Cough" image_url="https://imageserver.com/lamo.png"
+
+
